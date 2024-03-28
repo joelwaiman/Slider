18ab911765abfaa21c740380a1735d8db8a1a2b4
joelwaiman <waimanjoel@gmail.com>
2024-03-28 13:53:55 -0300

    mobile view

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -9,15 +9,15 @@ const socialIcons = [
 export function Footer() {
     return (
         <footer className="flex justify-between items-center w-[90vw] h-[5vh]">
-            <div className="flex justify-between w-[15%] text-xl">
+            <div className="flex justify-between sm:w-[15%] w-[30%] text-xl">
                 {socialIcons.map((icon) => {
-                    return <a className="hover:scale-[1.175] hover:text-[#AA895f] duration-300"
+                    return <a className="hover:scale-[1.175] hover:text-[#708f96] duration-300"
                     key={icon}
                         href="/">{icon}</a>
                 })}
             </div>
 
-            <a className="text-xl font-display hover:scale-[1.175] hover:text-[#AA895f] duration-300" 
+            <a className="text-xl font-display hover:scale-[1.175] hover:text-[#708f96] duration-300" 
             href="/">About us</a>
         </footer>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -12,10 +12,10 @@ export default function Home() {
   const [currentSlider, setCurrentSlider] = useState(0);
 
   const slider = [
-    { title: 'ICELAND', url: '/iceland-bg.webp' },
-    { title: 'MALDIVES', url: '/maldives-bg.webp' },
-    { title: 'THAILAND', url: '/thailand-bg.webp' },
-    { title: 'COSTA RICA', url: '/costa-rica-bg.webp' },
+    { title: 'ICELAND', image: '/iceland-bg.webp' },
+    { title: 'MALDIVES', image: '/maldives-bg.webp' },
+    { title: 'THAILAND', image: '/thailand-bg.webp' },
+    { title: 'COSTA RICA', image: '/costa-rica-bg.webp' },
   ]
 
   const nextSlide = () => {
@@ -36,41 +36,42 @@ export default function Home() {
 
   return (
     <main className="min-h-screen bg-cover bg-no-repeat duration-500"
-      style={{ backgroundImage: `url(${slider[currentSlider].url}` }}>
+      style={{ backgroundImage: `url(${slider[currentSlider].image}` }}>
 
-      <section className="grid grid-rows-3-[60px, 1fr, 60px] place-items-center gap-10 min-h-screen bg-gradient-to-t from-gray-950 from-1% to-transparent to-80%">
+      <section className="grid md:grid-rows-3-[60px, 1fr, 60px] grid-rows-3-[20px, 1fr, 20px] place-items-center sm:gap-10 min-h-screen bg-gradient-to-t from-gray-950 from-1% to-transparent to-80%">
 
         <img className="w-[75px]"
           src="/logo.webp" alt="logo" />
 
         <div className="flex flex-col items-center">
 
-          <div className="flex items-center group">
+          <div className="flex items-center justify-center gap-44 group w-[100vw]">
             <IoIosArrowBack
-              className="hidden group-hover:block hover:text-gray-50 cursor-pointer w-[20vh] text-gray-50/60 min-h-[25vh] animate-fade"
+              className="hidden sm:group-hover:block hover:text-gray-50/80 cursor-pointer w-[20vh] text-gray-50/40 min-h-[30vh] animate-fade"
               onClick={prevSlide} />
 
-            <p className="font-title w-[761px] text-center text-[150px] tracking-[7px] m-4 animate-fade-down"
+            <p className="font-title content-center w-auto h-[240px] text-center text-[80px] sm:text-[150px] tracking-[7px] sm:m-4 animate-fade-down"
               key={currentSlider}>
               {slider[currentSlider].title}
             </p>
 
             <IoIosArrowForward
-              className="hidden group-hover:block hover:text-gray-50 cursor-pointer w-[20vh] text-gray-50/60 min-h-[30vh] animate-fade"
+              className="hidden sm:group-hover:block hover:text-gray-50/80 cursor-pointer w-[20vh] text-gray-50/40 min-h-[30vh] animate-fade"
               onClick={nextSlide} />
           </div>
 
-          <button className="bg-gradient-to-tr hover:bg-gradient-to-tl from-[#708f96] to-[#364649] py-1.5 px-6 rounded-3xl w-[150px]">
+          <button className="bg-[#364649] hover:bg-[#708f96] transition duration-300 ease-out py-1.5 px-6 rounded-3xl w-[150px]">
             EXPLORE
           </button>
 
-          <div className="flex top-4 justify-center p-4">
+          <div className="flex top-4 justify-center py-4">
             {slider.map((slide, slideIndex) => {
               return <div
                 key={slide}
                 onClick={() => goToSlide(slideIndex)}
-                className={slideIndex === currentSlider ? "flex items-center justify-center w-[100px] text-9xl cursor-pointer transition-all duration-500 h-20" : "flex items-center justify-center w-[100px] text-gray-50/50 text-5xl cursor-pointer transition-all duration-500 h-20"}>
-                <RxDotFilled />
+                className="sm:w-[100px] w-[50px] flex items-center justify-center"
+                >
+                <RxDotFilled className={slideIndex === currentSlider ? "text-6xl cursor-pointer transition-all duration-500 h-20" : "text-gray-50/50 text-3xl cursor-pointer transition-all duration-500 h-20"} />
               </div>
             })}
           </div>
